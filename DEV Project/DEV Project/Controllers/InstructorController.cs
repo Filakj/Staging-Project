a0cb8daafdc4c6783c1751ea0ca99c4676355473
filakj <john.m.filak@gmail.com>
2020-03-27 09:34:09 -0400

    instructor controller logic

--- DEV Project/DEV Project/Controllers/InstructorController.cs
+++ DEV Project/DEV Project/Controllers/InstructorController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DEV_Library.Interface;
+using DEV_Library.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace DEV_Project.Controllers
 {
@@ -11,38 +14,61 @@ namespace DEV_Project.Controllers
     [ApiController]
     public class InstructorController : ControllerBase
     {
-        /*
-        // GET: api/Instructor
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private readonly ILogger<InstructorController> _logger;
+        private readonly IInstructorRepository _instructorRepository; 
+
+        public InstructorController(ILogger<InstructorController> logger, IInstructorRepository instructorRepository)
         {
-            return new string[] { "value1", "value2" };
+            _logger = logger;
+            _instructorRepository = instructorRepository; 
         }
+
+        
 
         // GET: api/Instructor/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        [HttpGet("{id}")]
+        public IActionResult GetInstructorById(Guid id)
         {
-            return "value";
+            Instructor instructor = _instructorRepository.GetInstructorById(id);
+            return Ok(instructor);
         }
 
+
+        
         // POST: api/Instructor
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult PostInstructor(Instructor instructor)
         {
+
+            _instructorRepository.AddInstructor(instructor);
+            _instructorRepository.SaveChanges();
+
+            Guid newId = new Guid();
+            instructor.Id = newId; 
+       
+            return CreatedAtAction(nameof(GetInstructorById), new { id = instructor.Id }, instructor);
         }
 
         // PUT: api/Instructor/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(Instructor instructor)
         {
+            _instructorRepository.UpdateInstructor(instructor);
+            _instructorRepository.SaveChanges();
+
+            return NoContent();
         }
 
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult DeleteInstructor(Guid id)
         {
+            _instructorRepository.DeleteInstructor(id);
+            _instructorRepository.SaveChanges();
+
+            return Content($"Account with id: {id} has been deleted.");
         }
-        */
+        
+
     }
 }
